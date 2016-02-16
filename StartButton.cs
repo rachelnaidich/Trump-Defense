457915f38efcf604741c8dee43fbceb471af3015
Rachel Naidich <rachelnaidich1@gmail.com>
2016-02-15 22:43:54 -0500

    Create StartButton.cs

---
+++ StartButton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void StartGame() {
+		Debug.Log ("hi");
+		SceneManager.LoadScene("GameScene");
+	}
+}
